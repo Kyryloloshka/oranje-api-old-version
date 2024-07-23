1fdb2ea017090d8b1c637891b2950dfdb2d2da50
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-07-23 21:36:49 +0300

    add crud for orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { OrderService } from './order.service';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
-@Controller('order')
-export class OrderController {}
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  getAll() {
+    return this.orderService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number) {
+    return this.orderService.getById(id);
+  }
+
+  @Post()
+  create(@GetUser() user: User) {
+    return this.orderService.create(user.id);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.orderService.delete(id);
+  }
+}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getAll() {
+    return this.prisma.order.findMany();
+  }
+
+  getById(id: number) {
+    return this.prisma.order.findUnique({ where: { id } });
+  }
+
+  create(userId: number) {
+    return this.prisma.order.create({
+      data: {
+        userId,
+      },
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.order.delete({ where: { id } });
+  }
+}
