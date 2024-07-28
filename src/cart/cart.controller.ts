9cd1f663819eb29314ec7e6dc13de215ef281e45
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-07-28 23:14:06 +0300

    implement crud for cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CartService } from './cart.service';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
-@Controller('cart')
-export class CartController {}
+@Controller('carts')
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Get()
+  getAll() {
+    return this.cartService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number) {
+    return this.cartService.getById(id);
+  }
+
+  @Post()
+  create(@GetUser() user: User) {
+    return this.cartService.create(user.id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.cartService.delete(id);
+  }
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(private prismaa: PrismaService) {}
+
+  getAll() {
+    return this.prismaa.cart.findMany();
+  }
+
+  getById(id: number) {
+    return this.prismaa.cart.findUnique({ where: { id } });
+  }
+
+  create(userId: number) {
+    return this.prismaa.cart.create({
+      data: {
+        userId,
+      },
+    });
+  }
+
+  delete(id: number) {
+    return this.prismaa.cart.delete({ where: { id } });
+  }
+}
